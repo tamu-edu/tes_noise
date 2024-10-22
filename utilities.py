aeb9df49f029d73b83c9d1d011ceabe4d38a55b1
Cryolab2 <Cryolab2@PHYS-MPHYB13-B.physics.tamu.edu>
2024-10-22 16:42:38 -0500

    added case to find_imports function

--- utilities.py
+++ utilities.py
@@ -17,16 +17,30 @@ def find_imports(maxdepth = 2):
     """
     function to find folders named "QETpy" and "optimal_filter" in 
     """
+    qp_found = False
+    of_found = False
+
+    def _get_stuff(checkdir):
+        if 'QETpy' in _os.listdir(checkdir):
+            _sys.path.append(checkdir + '/QETpy')
+            qp_found = True        
+        if 'QETpy' in _os.listdir(checkdir):
+            _sys.path.append(checkdir + '/optimal_filter')
+            of_found = True
 
     for d in range(maxdepth+1):
 
+        if qp_found and of_found:
+            return
+
         checkdir = '/'.join(d*['..']) if d  else '.'
 
-        if 'QETpy' in _os.listdir(checkdir):
-            _sys.path.append(checkdir + '/QETpy')
-        
-        if 'QETpy' in _os.listdir(checkdir):
-            _sys.path.append(checkdir + '/optimal_filter')
+        _get_stuff(checkdir)
+
+        if 'Documents' in _os.listdir(checkdir):
+            checkdir += '/Documents'
+            _get_stuff(checkdir)
+
 
 
 def find_folder(folder, *candidates):
